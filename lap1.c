b5cbf209ac4224ca4e7bc27440a3aea018710e85
6704062611247 <s6704062611247@email.kmutnb.ac.th>
2024-11-28 14:36:21 +0700

    lap1 work2

--- lap1.c
+++ lap1.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 int main() {
-	int num1 = 5;
-	int num2 = 3;
+	int num1;
+	int num2;
+	
+	scanf("%d",&num1);
+	scanf("%d",&num2);
 	
 	if (num1>num2) {
 		printf("num1 greater than num2");
