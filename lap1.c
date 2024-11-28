0aaca9248a0a817335211c3f0cfb4ee3e85bb1b5
6704062611247 <s6704062611247@email.kmutnb.ac.th>
2024-11-28 14:48:41 +0700

    lap1 work2 update

--- lap1.c
+++ lap1.c
@@ -2,13 +2,18 @@
 int main() {
 	int num1;
 	int num2;
+	int sum;
 	
 	scanf("%d",&num1);
 	scanf("%d",&num2);
 	
 	if (num1>num2) {
-		printf("num1 greater than num2");
+		printf("num1 greater than num2 \n");
+		sum = num1 - num2;
+		printf("%d",sum);
 	} else {
-		printf("num2 greater than num1");
+		printf("num2 greater than num1 \n");
+		sum = num2 - num1;
+		printf("%d",sum);
 	}
 }
