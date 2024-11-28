e2f60b8d4f06a5db8e14c5568a36f7d67f28cc97
6704062611247 <s6704062611247@email.kmutnb.ac.th>
2024-11-28 14:42:32 +0700

    lap1 work4

--- lap1.c
+++ lap1.c
@@ -3,8 +3,9 @@ int main() {
 	int num1;
 	int num2;
 	
-	scanf("%d" "%d",&num1, &num2);
-
+	scanf("%d",&num1);
+	scanf("%d",&num2);
+	
 	if (num1>num2) {
 		printf("num1 greater than num2");
 	} else {
