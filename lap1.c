8b9d6de1888fd5b7bc9e43ac883ee1f1e56dbc9a
6704062611247 <s6704062611247@email.kmutnb.ac.th>
2024-11-28 14:21:37 +0700

    lap1update

--- lap1.c
+++ lap1.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 int main() {
-	int num1 = 2;
+	int num1 = 5;
 	int num2 = 3;
 	
 	if (num1>num2) {
 		printf("num1 greater than num2");
-	}
-	if (num2>num1) {
+	} else {
 		printf("num2 greater than num1");
 	}
 }
